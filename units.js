5d4d793174822b4aef935410895f8a0a8e8fbb0f
Gurpreet Atwal <git@gatwal.com>
2016-12-15 01:26:33 -0800

    install eslint and lint code

--- units.js
+++ units.js
@@ -1,85 +1,87 @@
+'use strict';
+
 module.exports = {
   in: {
     base: 1000 / 0.3048 * 12,
-    aliases: ['inch', 'inches']
+    aliases: ['inch', 'inches'],
   },
   ft: {
     base: 1000 / 0.3048,
-    aliases: ['foot', 'feet']
+    aliases: ['foot', 'feet'],
   },
   mi: {
-    base: 1/1.609344,
-    aliases: ['mile', 'miles']
+    base: 1 / 1.609344,
+    aliases: ['mile', 'miles'],
   },
   cm: {
     base: 100000,
-    aliases: ['centimeter', 'centimeters']
+    aliases: ['centimeter', 'centimeters'],
   },
   m: {
     base: 1000,
-    aliases: ['meter', 'meters', 'metre', 'metres' ]
+    aliases: ['meter', 'meters', 'metre', 'metres'],
   },
   km: {
     base: 1,
-    aliases: ['kilometer', 'kilometers']
+    aliases: ['kilometer', 'kilometers'],
   },
   psi: {
     base: 0.145038,
-    aliases: ['psis']
+    aliases: ['psis'],
   },
   pa: {
     base: 1000,
-    aliases: ['pascal', 'pascals']
+    aliases: ['pascal', 'pascals'],
   },
   bar: {
     base: 0.01,
-    aliases: ['bars']
+    aliases: ['bars'],
   },
   mbar: {
     base: 10,
-    aliases: ['millibar', 'millibars', 'mbars', 'hectopascal', 'hectopascals']
+    aliases: ['millibar', 'millibars', 'mbars', 'hectopascal', 'hectopascals'],
   },
   mmhg: {
     base: 7.50061561303,
-    aliases: ['torr']
+    aliases: ['torr'],
   },
   atm: {
     base: 0.00986923,
-    aliases: ['atmosphere', 'atmospheres', 'atmospheric', 'barometric']
+    aliases: ['atmosphere', 'atmospheres', 'atmospheric', 'barometric'],
   },
   kpa: {
     base: 1,
-    aliases: ['kpas', 'kilopascal', 'kilopascals']
+    aliases: ['kpas', 'kilopascal', 'kilopascals'],
   },
   k: {
-    from: function(c){ return c + 273.15; },
-    to: function(k){ return k - 273.15; },
-    aliases: ['kelvin', 'kelvins']
+    from: function(c) { return c + 273.15; },
+    to: function(k) { return k - 273.15; },
+    aliases: ['kelvin', 'kelvins'],
   },
   f: {
-    from: function(c){ return c * 9/5 + 32; },
-    to: function(f){ return (f - 32) * 5/9; },
-    aliases: ['fahrenheit']
+    from: function(c) { return (c * 9 / 5) + 32; },
+    to: function(f) { return (f - 32) * 5 / 9; },
+    aliases: ['fahrenheit'],
   },
   c: {
-    from: function(c){ return c; },
-    to: function(c){ return c; },
-    aliases: ['celcius', 'centigrade']
+    from: function(c) { return c; },
+    to: function(c) { return c; },
+    aliases: ['celcius', 'centigrade'],
   },
   oz: {
     base: 35.274,
-    aliases: ['ounce', 'ounces']
+    aliases: ['ounce', 'ounces'],
   },
   lb: {
     base: 2.20462,
-    aliases: ['pound', 'lbs', 'pounds']
+    aliases: ['pound', 'lbs', 'pounds'],
   },
   g: {
     base: 1000,
-    aliases: ['gm', 'gram', 'grams']
+    aliases: ['gm', 'gram', 'grams'],
   },
   kg: {
     base: 1,
-    aliases: ['kilogram', 'kilograms']
-  }
+    aliases: ['kilogram', 'kilograms'],
+  },
 };
